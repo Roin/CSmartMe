f4ae81b03682f1f8fd4b029dc56600a3171ebcde
Florian Kanngießer <florian.kanngiesser@yahoo.de>
2013-02-20 18:27:16 +0100

    fixed syntax error

--- mysite/csm/views.py
+++ mysite/csm/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.templates import Context, Loader
+from django.template import Context, Loader
 from django.utils import simplejson
 from csm.models import Data
 
