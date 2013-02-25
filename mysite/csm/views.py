d370ec41fc86921ff94046bf2eb383956f9090b0
Florian Kanngießer <florian.kanngiesser@yahoo.de>
2013-02-25 21:56:58 +0100

    pew

--- mysite/csm/views.py
+++ mysite/csm/views.py
@@ -21,7 +21,7 @@ def index(request):
 @csrf_exempt
 def stuff(request):
   print datetime.datetime.now().day
-  mylist = Data.objects.filter(pub_date__day='25')[len(mylist)-10:len(mylist)].values()
+  mylist = Data.objects.filter(pub_date__day='25')[len(mylist)-10:len(mylist)-1].values()
   print "Da"
   print "Hier"
   message = {"L1" : [], "L2": [], "L3": [], "Sum": [], "PubDate" : []} 
