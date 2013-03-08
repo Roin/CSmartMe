05c3351ddc05ba02813ae1c0fa20e43b2b993761
Florian Kanngießer <florian.kanngiesser@yahoo.de>
2013-03-08 18:00:39 +0100

    still fiddling with cache

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,14 +131,14 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'unique-snowflake',
         'OPTIONS': {
-	  'MAX_ENTRIES': 100
+	  'MAX_ENTRIES': 10000
 	} 
     }
 }
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
+# See http://docs.djangoproject.com/en/dev/topics/logging for  	
 # more details on how to customize your logging configuration.
 #LOGGING = {
 #    'version': 1,
