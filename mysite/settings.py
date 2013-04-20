dd73f75f9c10139732be3fc0fe098c3ca9614c56
Florian Kanngießer <florian.kanngiesser@yahoo.de>
2013-04-20 16:25:45 +0200

    added static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = 'http://192.168.8.3:8000/home/florian/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/florian/djangostatic/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
